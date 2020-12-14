8de75fe8264bcb0934b030f7031e55024ea5fa14
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2020-12-14 11:28:37 +0100

    First Drop

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Class1: IClass
+    {
+        public void TestMethod()
+        {
+            System.Console.WriteLine("Holi");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IClass test = new Class1();
+            test.TestMethod();            
+        }
+    }
+}
